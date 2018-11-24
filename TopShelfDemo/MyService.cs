768ea3a763543cdd0c9f9d460103d7a0bebd05f0
builderman <builderman@126.com>
2018-11-24 18:15:01 +0800

    Add Log4Net

---
+++ TopShelfDemo/MyService.cs
@@ -0,0 +1,28 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopShelfDemo
+{
+    public class MyService1
+    {
+        private readonly ILog _logger; // = LogManager.GetLogger("abc");
+        public MyService1(ILog logger)
+        {
+            _logger = logger;
+        }
+
+        public void Start()
+        {
+            _logger.Info("Start ---");
+        }
+        public void Stop()
+        {
+            _logger.Info("Stop ---");
+        }
+
+    }
+}
--- TopShelfDemo/Program.cs
+++ TopShelfDemo/Program.cs
@@ -3,13 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Unity;
+using Unity.log4net;
+using Topshelf;
+using log4net;
+using log4net.Config;
+using System.IO;
 
 namespace TopShelfDemo
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            var container = CreateContainer();
+
+            var exitCode =  HostFactory.Run(hostConfigurator=> {
+                hostConfigurator.UseLog4Net();
+                hostConfigurator.SetDescription("This is a description");
+                hostConfigurator.SetDisplayName("DisplayName");
+                hostConfigurator.SetServiceName("ServiceName");
+
+                hostConfigurator.Service<MyService1>(serviceConfigurator=> {
+                    serviceConfigurator.ConstructUsing(p => container.Resolve<MyService1>());
+                    serviceConfigurator.WhenStarted(p => p.Start());
+                    serviceConfigurator.WhenStopped(p => p.Stop());
+                }).RunAsLocalSystem();
+            });
         }
+
+        public static IUnityContainer CreateContainer()
+        {
+            XmlConfigurator.Configure(new FileInfo("Log4Net.config"));
+
+            var container = new UnityContainer();
+            container.AddNewExtension<Log4NetExtension>();
+            container.RegisterSingleton<MyService1>();
+
+            return container;
+        }
+
+
+
     }
 }
